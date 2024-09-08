93d90d030a35de8d4657071bae634c7bb0303329
doom <61824552+JustDoom@users.noreply.github.com>
2024-09-08 20:28:53 +1000

    Allow basic text errors. Good for short errors

--- src/main/java/com/imjustdoom/minecrash/command/impl/CrashCmd.java
+++ src/main/java/com/imjustdoom/minecrash/command/impl/CrashCmd.java
@@ -5,6 +5,7 @@ import com.imjustdoom.minecrash.util.CrashUtil;
 import com.imjustdoom.minecrash.util.NetworkUtil;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
@@ -30,7 +31,7 @@ public class CrashCmd implements Command {
 
     @Override
     public OptionData[] getOptions() {
-        return new OptionData[]{new OptionData(OptionType.ATTACHMENT, "error", "The file containing the error", true)};
+        return new OptionData[]{new OptionData(OptionType.ATTACHMENT, "error", "The file containing the error", false), new OptionData(OptionType.STRING, "errortext", "Text containing a short error", false)};
     }
 
     @Override
@@ -46,58 +47,83 @@ public class CrashCmd implements Command {
     @Override
     public void execute(SlashCommandInteractionEvent event) {
 
-        Message.Attachment errorFile = event.getOption("error").getAsAttachment();
-
-        // Convert to MiB
-        if ((errorFile.getSize() / 1024f / 1024f) > 24f) {
-            event.reply("File is too large, currently only 24MiB and below are supported").queue();
-            return;
-        }
-
-        if (errorFile.getContentType() == null || !errorFile.getContentType().contains("text/plain")) {
-            event.reply("Failed to read file. Please upload a .txt file containing your error").queue();
-            return;
-        }
+        OptionMapping fileOption = event.getOption("error");
+        OptionMapping textOption = event.getOption("errortext");
 
         event.deferReply().queue();
-        CompletableFuture<InputStream> futureFile = errorFile.getProxy().download();
 
-        futureFile.whenComplete((inputStream, e) -> {
-            String text = "";
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                StringBuilder out = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    out.append(line);
-                }
-                text = out.toString();
-            } catch (IOException exception) {
-                event.getHook().sendMessage("Error").queue();
+        if (fileOption != null) {
+            Message.Attachment errorFile = fileOption.getAsAttachment();
+            // Convert to MiB
+            if ((errorFile.getSize() / 1024f / 1024f) > 24f) {
+                event.reply("File is too large, currently only 24MiB and below are supported").queue();
                 return;
             }
 
-            try {
-                String[] response = NetworkUtil.sendErrorForCheck(text);
-
-                if (response.length == 1) {
-                    event.getHook().sendMessageEmbeds(CrashUtil.getDefaultEmbed()
-                            .setTitle("Unknown Crash/Error")
-                            .setDescription(response[0])
-                            .setColor(Color.ORANGE)
-                            .build()).queue();
-                } else {
-                    event.getHook().sendMessageEmbeds(CrashUtil.getDefaultEmbed()
-                            .setTitle(response[0])
-                            .setDescription(response[1])
-                            .setColor(Color.GREEN)
-                            .build()).queue();
-                }
-            } catch (IOException ex) {
-                event.getHook().sendMessageEmbeds(CrashUtil.getErrorEmbed().build()).queue();
-                ex.printStackTrace();
+            if (errorFile.getContentType() == null || !errorFile.getContentType().contains("text/plain")) {
+                event.reply("Failed to read file. Please upload a .txt file containing your error").queue();
+                return;
             }
-        });
+
+            CompletableFuture<InputStream> futureFile = errorFile.getProxy().download();
+            futureFile.whenComplete((inputStream, e) -> {
+                String text = "";
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder out = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        out.append(line);
+                    }
+                    text = out.toString();
+                } catch (IOException exception) {
+                    event.getHook().sendMessage("Error").queue();
+                    return;
+                }
+
+                checkError(event, text);
+            });
+
+            return;
+        }
+
+        if (textOption != null) {
+            String error = textOption.getAsString();
+
+            if (textOption.getAsString().isBlank()) {
+                event.reply("Error is blank, please try again with an error specified.").queue();
+                return;
+            }
+
+            checkError(event, error);
+
+            return;
+        }
+
+        event.getHook().sendMessageEmbeds(CrashUtil.getDefaultEmbed().setTitle("Please specify either an error file or an error through text using the \"errortext\" option.").build()).queue();
+    }
+
+    private void checkError(SlashCommandInteractionEvent event, String error) {
+        try {
+            String[] response = NetworkUtil.sendErrorForCheck(error);
+
+            if (response.length == 1) {
+                event.getHook().sendMessageEmbeds(CrashUtil.getDefaultEmbed()
+                        .setTitle("Unknown Crash/Error")
+                        .setDescription(response[0])
+                        .setColor(Color.ORANGE)
+                        .build()).queue();
+            } else {
+                event.getHook().sendMessageEmbeds(CrashUtil.getDefaultEmbed()
+                        .setTitle(response[0])
+                        .setDescription(response[1])
+                        .setColor(Color.GREEN)
+                        .build()).queue();
+            }
+        } catch (IOException ex) {
+            event.getHook().sendMessageEmbeds(CrashUtil.getErrorEmbed().build()).queue();
+            ex.printStackTrace();
+        }
     }
 
     @Override
